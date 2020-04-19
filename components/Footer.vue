ce7ab176f15d3790479adb747ebd0651efeafc76
Joel Hernandez <joel@open-book.org>
2020-04-19 21:08:26 +0200

    :sparkles: add link to github on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,24 @@
 <template>
-  <footer class="footer has-background-black-bis">
+  <footer class="footer has-background-black-bis has-padding-30">
     <div class="content has-text-centered">
-      <p>
-        <a href="https://joelhernandez.xyz" rel="nofollow noreferrer" class="has-text-white" target="_blank">
-          © Joel Hernández 2020
+      <p class="has-text-white">
+        <span>An experiment by </span>
+        <a href="https://joelhernandez.xyz" rel="nofollow noreferrer" target="_blank" class="has-text-white">
+          Joel Hernández
         </a>
+        <span> with data from </span>
+        <a href="https://www.themealdb.com" rel="nofollow noreferrer" target="_blank" class="has-text-white">
+          TheMealDB
+        </a>
+        <span>.</span>
       </p>
+    </div>
+    <div class="container has-text-centered">
+      <span class="icon has-text-white">
+        <a href="https://github.com/lifenautjoe/meallie" rel="nofollow noreferrer" target="_blank" class="has-text-white">
+          <font-awesome-icon :icon="['fab', 'github']"/>
+        </a>
+      </span>
     </div>
   </footer>
 </template>
