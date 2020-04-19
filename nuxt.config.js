fda69572cf7b51bb883aabd846938e26e0bf3b09
Joel Hernandez <joel@open-book.org>
2020-04-19 21:40:13 +0200

    :package: add SEO tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+      {hid: 'description', name: 'description', content: 'Fresh meals everyday.'}
     ],
     link: []
   },
